23aabaadd76e19868e5a38c21e145d7ff793bc56
amumus <1013760411@qq.com>
2019-02-16 13:46:49 +0800

    提交头部和脚部

---
+++ src/components/common/BaseFooter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      脚部
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "base-footer"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/common/BaseHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>头部</div>
+</template>
+
+<script>
+    export default {
+        name: "base-header"
+    }
+</script>
+
+<style scoped>
+
+</style>
